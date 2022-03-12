315d7d25f58dba6c1955ff614fb8cafbf4a4b727
ischanx <ischanx@foxmail.com>
2022-03-12 21:25:10 +0800

    feat: 引入组件库antd

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx({})],
+  plugins: [
+    vue(),
+    vueJsx({}),
+    Components({
+      dts: true,
+      resolvers: [AntDesignVueResolver()],
+    }),
+  ],
 })
