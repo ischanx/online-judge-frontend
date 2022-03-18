48ecb21adc7b2f3c9748d1f2f75d5f14513e6ad3
ischanx <ischanx@foxmail.com>
2022-03-19 00:03:33 +0800

    chore: 新增路径别名src -> @

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,13 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
   plugins: [
     vue(),
     vueJsx({}),
