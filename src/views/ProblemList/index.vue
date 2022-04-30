12b56231417369c41d336e6ea3d0ec0bc78feb0a
ischanx <ischanx@foxmail.com>
2022-05-01 00:03:25 +0800

    feat: 题目列表增加通过率显示

--- src/views/ProblemList/index.vue
+++ src/views/ProblemList/index.vue
@@ -22,16 +22,25 @@ const columns = [
     title: '难度',
     dataIndex: 'difficulty',
     key: 'difficulty',
+    align: 'center',
+  },
+  {
+    title: '提交',
+    dataIndex: 'totalSubmit',
+    key: 'submitStatus',
+    align: 'center',
   },
   {
     title: '创建时间',
     dataIndex: 'createTime',
     key: 'createTime',
+    align: 'center',
   },
   {
     title: '更新时间',
     dataIndex: 'updateTime',
     key: 'updateTime',
+    align: 'center',
   },
 ];
 const jumpToProblemPage = (problemId: number) => {
@@ -42,8 +51,20 @@ const jumpToProblemPage = (problemId: number) => {
     },
   });
 };
-const handleTitleClick = (e: PointerEvent,item: Problem) => {
+const handleTitleClick = (e: PointerEvent, item: Problem) => {
   jumpToProblemPage(item.id);
+};
+
+
+const cssStr = (error:number, total: number)=>{
+  const percent = 100* (total - error)/total || 1; // 等于0时随便传一个非零值，否则样式不展示
+  return 'background: linear-gradient(\n' +
+      '                  to right,\n' +
+      `                  rgba(225, 248, 227) ${percent}%,\n` +
+      `                  rgba(255, 245, 245) ${percent}%,\n` +
+      '                  rgba(255, 245, 245) 100%,\n' +
+      '                  rgba(0, 0, 0, 0) 100%\n' +
+      '                );';
 };
 </script>
 
@@ -51,7 +72,7 @@ const handleTitleClick = (e: PointerEvent,item: Problem) => {
   <a-row justify="center">
     <a-col :xs="23" :sm="23" :md="20" :lg="18" :xl="18">
       <h1 class="title">Problems</h1>
-      <a-table :columns="columns" :data-source="list">
+      <a-table :columns="columns" :data-source="list" row-class-name="problem-list-item">
         <template #bodyCell="{ column, record }">
           <template v-if="column.key === 'id'">
             <a-button type="link" @click="(e) => handleTitleClick(e, toRaw(record))">
@@ -70,8 +91,17 @@ const handleTitleClick = (e: PointerEvent,item: Problem) => {
           </template>
           <template v-else-if="column.key === 'difficulty'">
             <span>
-              {{  difficultyText[record.difficulty || 0 ] }}
+              {{ difficultyText[record.difficulty || 0] }}
             </span>
+          </template>
+          <template v-else-if="column.key === 'submitStatus'">
+            <div
+              v-show="record.totalSubmit"
+              class="submitStatus"
+              :style="cssStr(record.errorSubmit, record.totalSubmit)"
+            >
+              {{ record.errorSubmit || 0 }} / {{ record.totalSubmit || 0 }}
+            </div>
           </template>
         </template>
       </a-table>
@@ -83,5 +113,17 @@ const handleTitleClick = (e: PointerEvent,item: Problem) => {
 .title {
   font-size: 30px;
   margin: 16px 0;
+}
+.problem-list-item {
+  .submitStatus {
+    height: 100%;
+    line-height: 50px;
+  }
+}
+</style>
+<style>
+.problem-list-item .ant-table-cell {
+  padding: 0;
+  height: 50px;
 }
 </style>
